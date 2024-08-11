a3dfd441b9ef0c0b5b7b8e868d1819b1aa662ba8
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2024-08-11 16:04:08 +0300

    ✨ add new feature for generate new Hadith in dashboard

--- app/Filament/Widgets/ActionsWidget.php
+++ app/Filament/Widgets/ActionsWidget.php
@@ -4,6 +4,8 @@ namespace App\Filament\Widgets;
 
 use Filament\Notifications\Notification;
 use Filament\Widgets\Widget;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Http;
 
 class ActionsWidget extends Widget
 {
@@ -11,12 +13,40 @@ class ActionsWidget extends Widget
 
     protected int|string|array $columnSpan = 2;
 
-    public function generateInvoice(): void
+    public function generateHadith(): void
     {
+        $hadith = null;
+        while (! $hadith) {
+            $hadith = $this->getHadith();
+        }
+
+        $name = data_get($hadith, 'id');
+        $number = data_get($hadith, 'contents.number');
+        $contents = data_get($hadith, 'contents.arab');
         Notification::make()
-            ->title('In progress successfully')
-            ->success()
-            ->body('Generating invoice.')
+            ->duration(null)
+            ->title("($name) - ($number)")
+            ->body($contents)
             ->send();
+    }
+
+    private function getHadith(): ?array
+    {
+        $books = $this->getBooks();
+        $book = Arr::random($books);
+        $name = $book['id'];
+        $number = rand(1, $book['available']);
+
+        return Http::get("https://api.hadith.gading.dev/books/$name/$number")
+            ->json('data');
+    }
+
+    private function getBooks(): array
+    {
+        return once(
+            function () {
+                return Http::get('https://api.hadith.gading.dev/books')->json('data');
+            }
+        );
     }
 }
--- resources/views/filament/widgets/invoice.blade.php
+++ resources/views/filament/widgets/invoice.blade.php
@@ -1,7 +1,7 @@
 <x-filament-widgets::widget>
     <x-filament::section>
         <div>
-            <x-filament::button wire:click="generateInvoice">Generate Invoice</x-filament::button>
+            <x-filament::button wire:click="generateHadith">New Hadith</x-filament::button>
         </div>
     </x-filament::section>
 </x-filament-widgets::widget>
